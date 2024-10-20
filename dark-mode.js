f732585c9c26aabbd2779edacf499e5af0d33c5d
Humphrey Shen <humphrey57shen@outlook.com>
2024-10-20 18:40:49 +0800

    dark theme added, rudimentary

---
+++ dark-mode.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+    document.body.classList.toggle('dark');
+    document.body.classList.toggle('light');
+    
+    /*const title = document.getElementsByClassName('title');
+    if (document.body.classList.contains('dark')) {
+      title.classList.remove('light-title');
+      title.classList.add('dark-title');
+    } else {
+      title.classList.remove('dark-title');
+    title.classList.add('light-title');*/
+    button = document.getElementById("dmbutton")
+    
+    button.src = button.src.endsWith("./Button/Light Mode.png") ? "./Button/Dark Mode.png" : "./Button/Light Mode.png";
+}
+    
